1079f2af7e7ab0ca8e2005757b7ba9cf712d4f81
Julian Escobar <julaxe8@gmail.com>
2020-09-30 15:41:40 -0400

    template physics

---
+++ GAME2005-F2020-Lab1-master/src/PlayScene.cpp
@@ -0,0 +1,64 @@
+#include "PlayScene.h"
+#include "Game.h"
+#include "EventManager.h"
+
+PlayScene::PlayScene()
+{
+	PlayScene::start();
+}
+
+PlayScene::~PlayScene()
+= default;
+
+void PlayScene::draw()
+{
+	drawDisplayList();
+}
+
+void PlayScene::update()
+{
+	updateDisplayList();
+
+	m_pDistanceLabel->setText("Distance = " + std::to_string(m_pPlayer->checkDistance(m_pEnemy)));
+}
+
+void PlayScene::clean()
+{
+	removeAllChildren();
+}
+
+void PlayScene::handleEvents()
+{
+	EventManager::Instance().update();
+
+	if (EventManager::Instance().isKeyDown(SDL_SCANCODE_A))
+	{
+		m_pPlayer->moveLeft();
+	}
+	else if (EventManager::Instance().isKeyDown(SDL_SCANCODE_D))
+	{
+		m_pPlayer->moveRight();
+	}
+
+	if (EventManager::Instance().isKeyDown(SDL_SCANCODE_ESCAPE))
+	{
+		TheGame::Instance()->quit();
+	}
+}
+
+void PlayScene::start()
+{
+	// Player Sprite
+	m_pPlayer = new Player();
+	addChild(m_pPlayer);
+	
+	// Enemy Sprite
+	m_pEnemy = new Enemy();
+	addChild(m_pEnemy);
+
+	// Label
+	const SDL_Color blue = { 0, 0, 255, 255 };
+	m_pDistanceLabel = new Label("Distance", "Consolas", 40, blue, glm::vec2(400.0f, 40.0f));
+	m_pDistanceLabel->setParent(this);
+	addChild(m_pDistanceLabel);
+}
